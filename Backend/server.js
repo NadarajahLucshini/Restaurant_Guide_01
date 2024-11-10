0c9e2ed6cb12d63a3bbaba686d6d8135a339cb27
nadarajah Lucshin <nadarajahlucshini@gmail.com>
2024-11-10 13:12:52 +0530

    add server file

---
+++ Backend/server.js
@@ -0,0 +1,32 @@
+// server.js
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+import db from './app/model/index.js';
+import authRoutes from './app/routes/auth.js';
+
+dotenv.config();
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(bodyParser.json());
+
+// Database connection
+db.connect();
+
+// Routes
+app.use('/api/auth', authRoutes);
+
+// Default route
+app.get('/', (req, res) => {
+  res.json({ message: 'Welcome to our simple API.' });
+});
+
+// Set port and start server
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
